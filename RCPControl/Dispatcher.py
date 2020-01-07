7ccb3c3b4e66824e68137f8880555895abcbf6f6
quanzeng-lc <quanzeng_lc@163.com>
2020-01-07 13:42:55 +0800

    hehe

--- RCPControl/Dispatcher.py
+++ RCPControl/Dispatcher.py
@@ -136,9 +136,9 @@ class Dispatcher(object):
 dispatcher = Dispatcher(1, 1)
 #dispatcher.agencyMotor.rm_move_to_position(12*2*1000, -4000*100*16)
 #time.sleep(70)
-#dispatcher.agencyMotor.rm_move_to_position(12*2*1000, 4000*100*16)
-#time.sleep(70)
-dispatcher.particleMotor.rm_move_to_position(8000, 4000*10)
-time.sleep(20)
+dispatcher.agencyMotor.rm_move_to_position(12*2*1000, 4000*100*14)
+time.sleep(70)
+#dispatcher.particleMotor.rm_move_to_position(8000, 4000*10)
+#time.sleep(20)
 dispatcher.stop_storing_data()
 
